89cfd6dadddcee63ff157679e19ba215a82b07b7
Zachary Smith <billyzacsmith@gmail.com>
2015-12-02 15:02:58 -0700

    Solution to 02.

--- 02-properties/base.js
+++ 02-properties/base.js
@@ -1,1 +1,10 @@
 
+module.exports = {
+  answerToLifeAndEverything: 42,
+  person: {
+    name: 'Joe',
+    age: 32
+  },
+  helloWorld: function(){return 'Hello world.'},
+  fishies: ['Cod', 'Salmon', 'Trout']
+}
